2706814db6ad90a8f401ef8eab320649a7a00271
pswierc <70979324+pswierc@users.noreply.github.com>
2020-09-08 14:43:25 -0600

    Add files via upload

---
+++ PHSX216_Plotting_2020-Copy1.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xheight_meters = np.array([1.103*2, 2.451*2, .717*2, .273*2, 1.466*2])
+
+xheight_meters = xheight_meters * 1000
+
+xheight_meters = np.round(xheight_meters)
+
+xheight_meters = xheight_meters * .001
+
+ytime_seconds = np.array([.44, .68, .37, .24, .52])
+
+ytime_seconds = ytime_seconds * 100
+
+ytime_seconds = np.round(ytime_seconds)
+
+ytime_seconds = ytime_seconds * .01
+
+uncertainty_g = np.array([.149, .062, .210, .3, .107])
+
+
+
+
+x = xheight_meters  
+y = ytime_seconds
+dy = uncertainty_g 
+
+#----------------------------------------------#
+#Don't need to change anything in this section!
+ 
+#Find the intercept and slope, b and m, from Python's polynomial fitting function
+b,m=np.polynomial.polynomial.polyfit(x,y,1,w=dy)
+
+#Write the equation for the best fit line based on the slope and intercept
+fit = b+m*x
+
+#Calculate the error in slope and intercept 
+#def Delta(x, dy) is a function, and we will learn how to write our own at a later date. They are very useful!
+def Delta(x, dy):
+    D = (sum(1/dy**2))*(sum(x**2/dy**2))-(sum(x/dy**2))**2
+    return D
+ 
+D=Delta(x, dy)
+ 
+dm = np.sqrt(1/D*sum(1/dy**2)) #error in slope
+db = np.sqrt(1/D*sum(x**2/dy**2)) #error in intercept
+
+#Calculate the "goodness of fit" from the linear least squares fitting document
+def LLSFD2(x,y,dy):
+    N = sum(((y-b-m*x)/dy)**2)
+    return N
+                      
+N = LLSFD2(x,y,dy)
+
+#-----------------------------------------------------------------------#
+#Plot data on graph. Plot error bars and place values for slope, error in slope
+#and goodness of fit on the plot using "annotate"
+
+plt.figure(figsize=(15,10))
+ 
+plt.plot(x, fit, color='green', linestyle='--')
+plt.scatter(x, y, color='blue', marker='o')
+ 
+ 
+#create labels  YOU NEED TO CHANGE THESE!!!
+plt.xlabel('Meters times 2')
+plt.ylabel('Seconds Squared')
+plt.title('Time Squared versus 2 Times Height')
+ 
+plt.errorbar(x, y, yerr=dy, xerr=None, fmt="none") #don't need to plot x error bars
+ 
+plt.annotate('Slope (s^2/m) = {value:.{digits}E}'.format(value=m, digits=2),
+             (0.05, 0.9), xycoords='axes fraction')
+ 
+plt.annotate('Error in Slope (s^2/m) = {value:.{digits}E}'.format(value=dm, digits=1),
+             (0.05, 0.85), xycoords='axes fraction')
+ 
+plt.annotate('Goodness of fit = {value:.{digits}E}'.format(value=N, digits=2),
+             (0.05, 0.80), xycoords='axes fraction')
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
